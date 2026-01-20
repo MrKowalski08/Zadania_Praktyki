8304915955d10fdfe3a1be2669bcd08e023544a4
radoo233 <radekgrzebelec183@gmail.com>
2026-01-20 09:51:27 +0100

    zamieniłem wynik na result ii poprawiłem w zakładce ip

--- Zadanie1/main.py
+++ Zadanie1/main.py
@@ -10,3 +10,20 @@ class Main:
             os.system(f"ping -n 1 {ip}")
 
 Main().run()
+import os
+
+class Main:
+    def __init__(self):
+        with open("ip.txt", "r") as f:
+            self.content = [line.strip() for line in f if line.strip()]
+
+    def run(self):
+        with open("result.txt", "w") as out:
+            for ip in self.content:
+                print(f"Pinguje: {ip}")
+                result = os.system(f"ping -n 1 {ip} > nul")
+
+                status = result == 0
+                out.write(f"{ip} -> {status}\n")
+
+Main().run()
