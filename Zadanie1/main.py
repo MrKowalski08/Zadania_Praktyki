bb4bb7f0604ffef1eb5ffeebb2a3e6b2ef1830be
MrKowalski08 <152518129+MrKowalski08@users.noreply.github.com>
2026-01-21 09:12:37 +0100

    added arguments

--- Zadanie1/main.py
+++ Zadanie1/main.py
@@ -1,25 +1,35 @@
 import subprocess
+import argparse
 
 class Main:
     def __init__(self):
         with open("ip.txt", "r") as f:
             self.content = [line.strip() for line in f if line.strip()]
+            self.parser = argparse.ArgumentParser()
+            
 
     def run(self):
-        with open("result.txt", "w") as out:
-            for ip in self.content:
-                print(f"Pinguje: {ip}")
-                try:
-                    subprocess.run(
-                        ["ping", "-n", "1", ip],
-                        stdout = subprocess.DEVNULL,
-                        stderr = subprocess.DEVNULL,
-                        check = True
-                    )
-                    status = True
-                except subprocess.CalledProcessError:
-                    status = False
-                
-                out.write(f"{ip} -> {status}\n")
+        self.parser.add_argument("-n", action="store_true", help="run normaly")
+        self.parser.add_argument("-r", action="store_true", help="rebooting in 5 min")
+        self.args = self.parser.parse_args()
+
+        if self.args.p:
+            with open("result.txt", "w") as out:
+                for ip in self.content:
+                    print(f"Pinguje: {ip}")
+                    try:
+                        subprocess.run(
+                            ["ping", "-n", "1", ip],
+                            stdout = subprocess.DEVNULL,
+                            stderr = subprocess.DEVNULL,
+                            check = True
+                        )
+                        status = True
+                    except subprocess.CalledProcessError:
+                        status = False
+                    
+                    out.write(f"{ip} -> {status}\n")
+
+        if self.args.r:
 
 Main().run()
