ba730326b4f7a9f57acdc62444aadf1df8ad1a6e
MrKowalski08 <152518129+MrKowalski08@users.noreply.github.com>
2026-01-28 12:38:39 +0100

    changed ui and added new functions

--- Zadanie2 tk/fancy_script.py
+++ Zadanie2 tk/fancy_script.py
@@ -2,6 +2,7 @@ import subprocess
 import time
 import tkinter as tk
 import xml.etree.ElementTree as ET
+import os
 
 class Fancy:
     def add_to_list(self, address, list):
@@ -9,6 +10,10 @@ class Fancy:
         if text:
             list.insert(tk.END, text)
             address.delete(0, tk.END)
+
+    def get_from_file(self, ip):
+        
+    
 
     def ping_ip(self,list):
         with open("result.txt", "w") as out:
@@ -28,8 +33,8 @@ class Fancy:
                 
                 out.write(f"{ip} -> {status}\n")
 
-    def reboot_ip(self,input):
-        ip = input.get()
+    def reboot_ip(self,i_ip):
+        ip = i_ip.get()
         while True:
             subprocess.run(["ping", "-n", "1", ip], 
                             stdout=subprocess.DEVNULL, 
@@ -38,15 +43,38 @@ class Fancy:
             subprocess.run(["adb", "reboot"])
             time.sleep(300)
 
-    def search_NetworkPref(self, input):
-        ip = input.get()
-        FILE_PATH = "/data/data/pl.inelo.assist/shared_prefs/NetworkPreferences.xml"
+    def search_NetworkPref(self, i_ip,i_path,i_name,callback):
+        ip = i_ip.get()
+        path = i_path.get()
+        name = i_name.get()
         subprocess.run(["adb", "connect", str(ip)])
         subprocess.run(["adb", "root"])
-        result = subprocess.run(["adb", "shell", "-n", "cat", FILE_PATH],
+        result = subprocess.run(["adb", "shell", "cat", path],
                     capture_output=True,
                     text=True,
         )
         data = ET.fromstring(result.stdout)
-        print(data.find(".//string[@name='fileServer']").text)
+        print(data.find(f".//string[@name='{name}']").text)
+        value = data.find(f".//string[@name='{name}']").text
+        callback(value)
         subprocess.run(["adb","disconnect"])
+
+
+    def search_File_based_ip(self, i_ip,i_path,i_name):
+        ips = i_ip.get(0, "end")
+        path = i_path.get()
+        name = i_name.get()
+
+        for ip in ips:
+            subprocess.run(["adb", "connect", str(ip)])
+            subprocess.run(["adb", "root"])
+
+            full_path = f"{path.rstrip('/')}/{name}"
+
+            result = subprocess.run(
+                ["adb", "shell", f"if [ -d '{full_path}' ] || [ -f '{full_path}' ]; then echo EXISTS; else echo NONE; fi"],
+                capture_output=True,
+                text=True,)
+            print(result.stdout)
+            subprocess.run(["adb","disconnect"])  
+
